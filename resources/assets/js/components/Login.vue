f668b59a2792c967be8ae30a5a37058aa2fc4916
yuweiwei <yy80072312@gmail.com>
2018-05-24 18:15:42 +0800

    yuweiwei add some ios meta and edit some config

--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -7,7 +7,7 @@
 
     <div class="login-body animated fadeIn" >
       <div class="col-choose-bar">
-        <div @click="isTeacherSelected = false" :id="isTeacherSelected? '':'selected'" class="bar student">學生</div>
+        <div @click="isTeacherSelected = false" :id="isTeacherSelected? '':'selected'" class="bar student">魚尾尾嗨嗨</div>
       <div @click="isTeacherSelected = true" :id="isTeacherSelected? 'selected':''" class="bar teacher">老師</div>
   </div>
 
@@ -31,58 +31,61 @@
 </template >
 
   <script>
-    export default {
-      props: ['swapComponent'],
-      data () {
-          return {
-            isTeacherSelected: false
-          }
-      }
-    }
+export default {
+  props: ["swapComponent"],
+  data() {
+    return {
+      isTeacherSelected: false
+    };
+  }
+};
 </script>
 
 
-  <style>
-    html,body,.container,#app{
-      width: 100%;
+<style>
+html,
+body,
+.container,
+#app {
+  width: 100%;
   height: 100%;
 }
 
-.container{
+.container {
   background: transparent url("/img/bg-img.png") no-repeat center;
   background-size: 90%;
 }
 
-.login-header{
+.login-header {
   display: flex;
-  align-items:center;
-  justify-content:center;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 35%;
 }
-.login-body{
-      width: 100%;
+.login-body {
+  width: 100%;
   height: 37%;
 }
-.login-bottom{
-      width: 100%;
+.login-bottom {
+  width: 100%;
   height: 30%;
 }
 
-.login-button{
-      display: flex;
-  align-items:center;
-  justify-content:center;
-  width:100%;
-  height:35%;
+.login-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 35%;
 }
 
-.logo{
-      height: 90%;
+.logo {
+  height: 90%;
 }
 
-.button-login{
-      font - weight: bold;
+.button-login {
+  font: bold;
   border-radius: 99em;
   width: 78%;
   height: 70%;
@@ -91,48 +94,71 @@
   font-family: Microsoft JhengHei;
   transition: opacity 2s;
   box-shadow: 6px 7px 20px 1px #a4a4a4;
-  background: rgb(248,80,50); /* Old browsers */
-background: rgb(252,234,187); /* Old browsers */
-background: -moz-linear-gradient(-45deg, rgba(252,234,187,1) 0%, rgba(252,205,77,1) 50%, rgba(248,181,0,1) 51%, rgba(251,223,147,1) 100%); /* FF3.6-15 */
-background: -webkit-linear-gradient(-45deg, rgba(252,234,187,1) 0%,rgba(252,205,77,1) 50%,rgba(248,181,0,1) 51%,rgba(251,223,147,1) 100%); /* Chrome10-25,Safari5.1-6 */
-background: linear-gradient(135deg, rgba(252,234,187,1) 0%,rgba(252,205,77,1) 50%,rgba(248,181,0,1) 51%,rgba(251,223,147,1) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fceabb', endColorstr='#fbdf93',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */}
+  background: rgb(248, 80, 50); /* Old browsers */
+  background: rgb(252, 234, 187); /* Old browsers */
+  background: -moz-linear-gradient(
+    -45deg,
+    rgba(252, 234, 187, 1) 0%,
+    rgba(252, 205, 77, 1) 50%,
+    rgba(248, 181, 0, 1) 51%,
+    rgba(251, 223, 147, 1) 100%
+  ); /* FF3.6-15 */
+  background: -webkit-linear-gradient(
+    -45deg,
+    rgba(252, 234, 187, 1) 0%,
+    rgba(252, 205, 77, 1) 50%,
+    rgba(248, 181, 0, 1) 51%,
+    rgba(251, 223, 147, 1) 100%
+  ); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(
+    135deg,
+    rgba(252, 234, 187, 1) 0%,
+    rgba(252, 205, 77, 1) 50%,
+    rgba(248, 181, 0, 1) 51%,
+    rgba(251, 223, 147, 1) 100%
+  ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient(
+      startColorstr="#fceabb",
+      endColorstr="#fbdf93",
+      GradientType=1
+    ); /* IE6-9 fallback on horizontal gradient */
+}
 
-.col-choose-bar{
-      display: flex;
-  align-items:center;
-  justify-content:center;
-  width:100%;
-  height:25%;
+.col-choose-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 25%;
 }
 
 .input {
-      display: flex;
-  align-items:center;
-  justify-content:center;
-  width:100%;
-  height:35%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 35%;
 }
 
-.col{
-      width: 75%;
+.col {
+  width: 75%;
   height: 60%;
   font-size: 3.5em;
-   text-indent: 3em;
+  text-indent: 3em;
 }
 
-#ac{
-      background: transparent url(/img/account.png) no-repeat 5%;
-    background-size: 9%;
+#ac {
+  background: transparent url(/img/account.png) no-repeat 5%;
+  background-size: 9%;
 }
 
-#pw{
-      background: transparent url(/img/password.png) no-repeat 4%;
-    background-size: 10%;
+#pw {
+  background: transparent url(/img/password.png) no-repeat 4%;
+  background-size: 10%;
 }
 
 .bar {
-      font - weight: bold;
+  font: bold;
   width: 30%;
   border-bottom: 8px solid #d9e1d8;
   height: 80%;
@@ -142,14 +168,28 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#fceabb', end
   color: #a3a1a1;
 }
 
-#selected{
-      color: #ff5f2e;
+#selected {
+  color: #ff5f2e;
   border-bottom: 8px solid #ff5f2e;
 }
-.effect-2{
-      border: 0; padding: 7px 0; border-bottom: 5px solid #ccc;
+.effect-2 {
+  border: 0;
+  padding: 7px 0;
+  border-bottom: 5px solid #ccc;
 }
 
-.effect-2 ~ .focus-border{position: absolute; bottom: 0; left: 50%; width: 0; height: 2px; background-color: #3399FF; transition: 0.4s;}
-.effect-2:focus ~ .focus-border{width: 100%; transition: 0.4s; left: 0;}
+.effect-2 ~ .focus-border {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  width: 0;
+  height: 2px;
+  background-color: #3399ff;
+  transition: 0.4s;
+}
+.effect-2:focus ~ .focus-border {
+  width: 100%;
+  transition: 0.4s;
+  left: 0;
+}
 </style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"  content="user-scalable=no">  
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Mix2Ideas攪攪腦">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" type="text/css" href="/css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="/css/animate.css" />
+    <title>Mix2Ideas攪攪腦－攪動你的想法吧！</title>
+</head>
+<body>
+    <!-- 準備給 Vue 的掛載點 -->
+    <div id="app">
+        <div :is="currentComponent" :swap-component="swapComponent"></div>
+    </div>
+    <!-- 載入打包後的 js 檔 -->
+    <script src="/js/hello.js"></script>
+</body>
+</html>
+<style>
+    @media (min-width: 400px) and (max-width: 1000px) {
+	html, body {
+		position: fixed;
+	}
+	
+	html, body {
+		position: relative;
+		overflow: hidden;
+	}
+	
+	body {
+		position: fixed;
+		overflow: hidden;
+	}
+	
+	body {
+		position: fixed;
+		height: 100%;
+		overflow: hidden;
+		width: 100%;
+		font-size: 1.0em;
+	}
+}
+</style>
